a65c7e0a2cd30785f9d396044e30d18466f8d99d
sups-vamp <supriya1880.cse16@chitakara.edu.in>
2020-04-12 14:14:09 +0530

    changes to profle component

--- Angular-pro/src/app/app.routing.ts
+++ Angular-pro/src/app/app.routing.ts
@@ -2,11 +2,17 @@
 
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
+import { CoursesComponent } from './courses';
+import { FeatureComponent } from './feature';
+import { ProfileComponent } from './profile';
 import { AuthGuard } from './_helpers';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
+    { path: 'courses', component: CoursesComponent },
+    { path: 'features', component: FeatureComponent },
+    { path: 'profile', component: ProfileComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
