72538cb76ade3f204ab261700b1a071746947626
sups-vamp <supriya1880.cse16@chitakara.edu.in>
2020-04-18 00:08:47 +0530

    welcome section + minor fixes

--- Angular-pro/src/app/home/home.component.ts
+++ Angular-pro/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { first } from 'rxjs/operators';
 
 import { UserService, AuthenticationService } from '../_services';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({ templateUrl: './home.component.html', styleUrls: ['./home.component.scss'] })
 export class HomeComponent implements OnInit {
     currentUser: any;
     users = [];
